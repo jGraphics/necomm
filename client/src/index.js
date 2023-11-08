39fa3615bee74905cc230110f25a3a6f9ec58404
jGraphics <jgraphics@gmail.com>
2023-11-08 09:36:23 -0800

    more fixes, one more bug to go

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from "react-dom/client"; 
+import { createRoot } from "react-dom/client"; 
 import { Provider } from 'react-redux'; 
 import ProductList from "./components/ProductList"; 
 import { BrowserRouter as Router } from 'react-router-dom'; 
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <Provider store={ProductList()}> 
   <React.StrictMode>
